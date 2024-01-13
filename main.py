c1fd003338eb5ae4aaf91c2cca7b4d34bc3de33a
Humadawari <156085415+Humadawari@users.noreply.github.com>
2024-01-13 05:56:59 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import  streamlit as st
+
+st.title("WELCOME")
+st.title("Here you can find information and youtube links of programming languages")
+
+st.markdown("A programming language is a language people use when developing software to tell a computer what to do. They come in many forms, but most programming languages rely on an interpreter that translates the human-readable language into binary so that the computer can put the instructions into action.")
+st.markdown("Programming languages are the reason we can do complex things with computers. At their core, computers are still operating on binary---also called machine language---a system where zeroes and ones determine what the computer does, and how. You can think of programming languages as a layer over this core, so humans don't need to toggle the zeroes into ones and back again.")
+st.header("Levels of Language")
+st.markdown("Roughly speaking, programming languages fall into two categories: low-level and high-level languages. Low-level languages are called that because they are closs to the machine, they can speak to it directly. This includes machine language and assembly languages, which are programming languages that are only a little removed from binary.")
+st.markdown("High-level languages are a step above low-level languages. They're further away from the machine, but are readable by humans. Readable in this case means that if you know the language in question you can look at a few lines of code and figure out what's going on. This also works the other way around: you can type up commands which will then be executed by the machine")
+st.header("How Programming Languages Work")
+st.markdown("The choice for the word language wasn't made by accident, either: just like in human languages, programming languages have internal rules that keep it all from going off the rails.")
+st.markdown("A programming language will have a syntax, a set of rules concerning word order and word use, just like in a human language. For example, in English you can say Gary gave Fred a book. In this sentence, you know exactly who gave what and to whom; change the words around and you get a different sentence: Fred gave Gary a book That still makes sense, but if you say a book to Gary Fred gave we have a problem on our hands.")
+st.markdown("Programming languages are no different: the right bits need to go into the right places for a sentence---usually called a line---to make sense. It's just that programming languages use different ways to express themselves.")
+st.subheader("PYTHON PROGRAMMING LANGUAGE")
+st.markdown("https://youtube.com/playlist?list=PLsyeobzWx|7poL9JTVyndKe62ieoN-MZ3&si=3L6WE00kapjKzrNQ")
+
+
+st.subheader("JAVASCRIPT PROGRAMMING LANGUAGE")
+st.markdown("https://youtu.be/W6NZfCO5SIk?si=TMFTtLJyBxV9RebM")
+
+
+st.subheader("C++ PROGRAMMING LANGUAGE")
+st.markdown("https://youtu.be/ZzaPdXTrSb8?si=XuuYInARv-GajSkm")
+
+
+st.subheader("JAVA PROGRAMMING LANGUAGE")
+st.markdown("https://youtu.be/eIrMbAQSU34?si=jTQr8JCZTInWBv0-")
+
+
+st.subheader("C PROGRAMMING LANGUAGE")
+st.markdown("https://youtu.be/KJgsSFOSQv0?si=ilZY__mb2-YMdGE-")
+
+st.subheader("RUBY PROGRAMMING LANGUAGE")
+st.markdown("https://youtu.be/t_ispmWmdjY?si=EFw4MAdJnr-eGd7G")
+
+
+st.subheader("C# PROGRAMMING LANGUAGE")
+st.markdown("https://youtu.be/gfkTfcpWqAY?si=sWxboAp00HMRBOmj")
+st.markdown("In short, a programming language is the way in which a computer programmer talks to a device. If you know how to speak one of these languages and there are hundreds you can create a program that can perform tasks. These can range from the very simple, like a script that moves a file from one place to another, to the very complex, like rendering a 3D world in a video game.")
+
+st.markdown("If you have any questions you can email me")
+st.markdown("humadawari@gmail.com")
